c8ed1ea43ea5507358dd18ee5993e73f16ba8cde
adrien-rana <76122065+adrien-rana@users.noreply.github.com>
2020-12-16 18:33:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    include './php/mysql.php';
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="./css/index.css">
+
+</head>
+<body>
+    <h1>Technical Test</h1>
+</body>
+</html>
+
